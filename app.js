9ec8ffff865897b5493be56224deff7fe9e22419
eudondian <eudondian@gmail.com>
2022-07-04 14:32:48 +0100

    Add html and js files

---
+++ app.js
@@ -0,0 +1,17 @@
+// Book Class
+
+// UI class
+class UI {
+    static displayBooks() {
+  
+      const books = StoredBooks;
+  
+  books.forEach((bok) => UI.addBookToList(book);
+  
+    }
+  }
+  
+  // Event:  Add a Book
+  
+  // Event: Remove a Book
+  
