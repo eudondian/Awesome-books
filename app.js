6a3d3cd94575b85184cad3d96dadce9b842d4555
Losalini <c.rokocakau@gmail.com>
2022-07-06 02:14:22 +1200

    Fix eslint linter errors

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const bookList = document.querySelector('.book-list');
 const bookTitle = document.querySelector('.book-title');
 const bookAuthor = document.querySelector('.book-author');
 const adbtn = document.querySelector('form');
-let bookCollectionArray = [];
+let bookArray = [];
 
-function removeABook(bookInfo, index){
-  let bookCollection2 = document.getElementById(index);
-  const { author: author, title: title } = bookInfo;
-  bookCollectionArray = bookCollectionArray.filter((item) => item.author !== author && item.title !== title);
-  localStorage.setItem('bookCollection', JSON.stringify(bookCollectionArray));
+function removeABook(bookInfo, index) {
+  const bookCollection2 = document.getElementById(index);
+  const { author, title } = bookInfo;
+  bookArray = bookArray.filter((item) => item.author !== author && item.title !== title);
+  localStorage.setItem('bookCollection', JSON.stringify(bookArray));
   bookList.removeChild(bookCollection2);
 }
 
-function addABook(bookInfo, index){
+function addABook(bookInfo, index) {
   const bookData = document.createElement('div');
   bookData.classList.add('bookData');
   bookData.id = index;
@@ -30,27 +30,27 @@ function addABook(bookInfo, index){
   };
 }
 
-function addBookItems(item){
-  bookCollectionArray.push({
-    
+function addBookItems(item) {
+  bookArray.push({
+
     title: bookTitle.value,
     author: bookAuthor.value,
   });
-  localStorage.setItem('bookCollection', JSON.stringify(bookCollectionArray));
+  localStorage.setItem('bookCollection', JSON.stringify(bookArray));
   bookTitle.value = '';
   bookAuthor.value = '';
-  addABook(item, bookCollectionArray.length - 1);
+  addABook(item, bookArray.length - 1);
 }
 
-function updateUI(){
-  if (localStorage.getItem('bookCollection')){
-    bookCollectionArray = JSON.parse(localStorage.getItem('bookCollection'));
-    bookCollectionArray.forEach((bookInfo, index) => {
-    addABook(bookInfo, index);
+function updateUI() {
+  if (localStorage.getItem('bookCollection')) {
+    bookArray = JSON.parse(localStorage.getItem('bookCollection'));
+    bookArray.forEach((bookInfo, index) => {
+      addABook(bookInfo, index);
     });
   } else {
     localStorage.setItem('bookCollection', '');
-    bookCollectionArray = [];
+    bookArray = [];
   }
 }
 
