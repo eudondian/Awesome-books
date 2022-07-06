05e85f137e50831d3468c780745db1c84c42c489
Losalini <c.rokocakau@gmail.com>
2022-07-07 02:22:58 +1200

    Add class of Book for awesome books

--- app.js
+++ app.js
@@ -1,65 +1,73 @@
+// Variable declarations
 const bookList = document.querySelector('.book-list');
 const bookTitle = document.querySelector('.book-title');
 const bookAuthor = document.querySelector('.book-author');
+let bookTitleValue;
+let bookAuthorValue;
 const adbtn = document.querySelector('form');
 let bookArray = [];
 
-function removeABook(bookInfo, index) {
-  const bookCollection2 = document.getElementById(index);
-  const { author, title } = bookInfo;
-  bookArray = bookArray.filter((item) => item.author !== author && item.title !== title);
-  localStorage.setItem('bookCollection', JSON.stringify(bookArray));
-  bookList.removeChild(bookCollection2);
-}
+// Represents a Book
+class Book {
+  static addBookItems(item){
+    bookArray.push(item);
+    console.log(bookArray);
 
-function addABook(bookInfo, index) {
-  const bookData = document.createElement('div');
-  bookData.classList.add('bookData');
-  bookData.id = index;
-  const removeBtn = document.createElement('button');
-  removeBtn.classList.add('remove-btn');
-  removeBtn.innerText = 'REMOVE';
-  bookData.innerHTML = `
-    <p class="book-title">${bookInfo.title}</p>
-    <p class="book-author">${bookInfo.author}</p>
-  `;
-  bookData.appendChild(removeBtn);
-  bookList.appendChild(bookData);
-  removeBtn.onclick = () => {
-    removeABook(bookInfo, index);
-  };
-}
+    localStorage.setItem('bookCollection', JSON.stringify(bookArray));
 
-function addBookItems(item) {
-  bookArray.push({
+    bookTitleValue = '';
+    bookAuthorValue = '';
+  }
 
-    title: bookTitle.value,
-    author: bookAuthor.value,
-  });
-  localStorage.setItem('bookCollection', JSON.stringify(bookArray));
-  bookTitle.value = '';
-  bookAuthor.value = '';
-  addABook(item, bookArray.length - 1);
-}
+  static addABook(bookInfo, index){
+    const bookData = document.createElement('div');
+    bookData.classList.add('bookData');
+    bookData.id = index;
+    const removeBtn = document.createElement('button');
+    removeBtn.classList.add('remove-btn');
+    removeBtn.innerText = 'REMOVE';
+    bookData.innerHTML = `
+      <p class="book-title">${bookInfo.title}</p>
+      <p class="book-author">${bookInfo.author}</p>
+    `;
+    bookData.appendChild(removeBtn);
+    bookList.prepend(bookData);
+    removeBtn.onclick = () => {
+      Book.removeABook(bookInfo, index);
+    };
+  }
 
-function updateUI() {
-  if (localStorage.getItem('bookCollection')) {
-    bookArray = JSON.parse(localStorage.getItem('bookCollection'));
-    bookArray.forEach((bookInfo, index) => {
-      addABook(bookInfo, index);
-    });
-  } else {
-    localStorage.setItem('bookCollection', '');
-    bookArray = [];
+  static removeABook(bookInfo, index){
+    const bookCollection2 = document.getElementById(index);
+    const { author, title } = bookInfo;
+    bookArray = bookArray.filter((item) => item.author !== author && item.title !== title);
+    localStorage.setItem('bookCollection', JSON.stringify(bookArray));
+    bookList.removeChild(bookCollection2);
   }
 }
 
-updateUI();
-
-adbtn.addEventListener('submit', (e) => {
-  e.preventDefault();
-  addBookItems({
-    title: bookTitle.value,
-    author: bookAuthor.value,
+// If local storage is not empty, display the items on the webpage
+/* if (localStorage.getItem('bookCollection')) {
+  bookArray = JSON.parse(localStorage.getItem('bookCollection'));
+  bookArray.forEach((bookInfo, index) => {
+    Book.addABook(bookInfo, index);
   });
-});
+} else {
+  localStorage.setItem('bookCollection', '');
+  bookArray = [];
+} */
+
+// Add a book event listener
+/* adbtn.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Get the form values
+  bookTitleValue = bookTitle.value;
+  bookAuthorValue = bookAuthor.value;
+
+  let item = { title: bookTitleValue, author: bookAuthorValue };
+
+  Book.addBookItems(item);
+
+  Book.addABook(item, bookArray.length - 1);
+}); */
