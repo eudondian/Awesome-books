0eaf7c6bb6a7dd02c0790de6ca5c25341eda8f02
Losalini <c.rokocakau@gmail.com>
2022-07-09 00:32:01 +1200

    Add code for Single Page application

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 // Variable declarations
+// For navbar 1 & navbar 2
+const list = document.querySelector('.book-list-link');
+const addNew = document.querySelector('.add-new-link');
+const contact = document.querySelector('.contact-link');
+const dateAndTime = document.querySelector('.date-and-time');
+let dateAndTime1 = new Date();
+
+// For Book list page
+const caption = document.querySelector('.caption');
 const bookList = document.querySelector('.book-list');
+
+// For Add new page
+const caption2 = document.querySelector('.caption2');
+const addContainer = document.querySelector('.add-container');
 const bookTitle = document.querySelector('.book-title');
 const bookAuthor = document.querySelector('.book-author');
 let bookTitleValue;
@@ -7,6 +20,9 @@ let bookAuthorValue;
 const addbtn = document.querySelector('form');
 let bookArray = [];
 const errorMsg = document.querySelector('.error-msg');
+
+// For Contact page
+const contactContainer = document.querySelector('.contact-container');
 
 // Represents a Book
 class Book {
@@ -88,40 +104,117 @@ class Book {
   }
 }
 
+// Remove all children elements of a parent node
+/* function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+  }
+} */
+
+dateAndTime.innerHTML = dateAndTime1;
+
 // If local storage is not empty, display the items on the webpage
-if (localStorage.getItem('bookCollection')) {
+/* if (localStorage.getItem('bookCollection')) {
   bookArray = JSON.parse(localStorage.getItem('bookCollection'));
 
   bookArray.forEach((item, index) => {
     Book.addABook(item, index);
   });
+
+  list.classList.add('blue');
+  addNew.classList.remove('blue');
+  contact.classList.remove('blue');
+  caption.classList.remove('hide');
+  bookList.classList.add('active');
+  addContainer.classList.add('hide');
+  contactContainer.classList.add('hide');
 } else {
   localStorage.setItem('bookCollection', '');
 
   bookArray = [];
+
+  list.classList.add('blue');
+  addNew.classList.remove('blue');
+  contact.classList.remove('blue');
+  caption.classList.remove('hide');
+  addContainer.classList.add('hide');
+  contactContainer.classList.add('hide');
+} */
+
+// List link
+list.onclick = () => {
+  if (localStorage.getItem('bookCollection')) {
+    bookArray = JSON.parse(localStorage.getItem('bookCollection'));
+
+    bookArray.forEach((item, index) => {
+      Book.addABook(item, index);
+    });
+
+    list.classList.add('blue');
+    addNew.classList.remove('blue');
+    contact.classList.remove('blue');
+    caption.classList.remove('hide');
+    bookList.classList.add('active');
+    addContainer.classList.add('hide');
+    contactContainer.classList.add('hide');
+  } else {
+    localStorage.setItem('bookCollection', '');
+  
+    bookArray = [];
+  
+    list.classList.add('blue');
+    addNew.classList.remove('blue');
+    contact.classList.remove('blue');
+    caption.classList.remove('hide');
+    addContainer.classList.add('hide');
+    contactContainer.classList.add('hide');
+  }
 }
 
-// Add a book event listener
-addbtn.addEventListener('submit', (e) => {
-  e.preventDefault();
+// Add new link
+addNew.onclick = () => {
+  list.classList.remove('blue');
+  contact.classList.remove('blue');
+  addNew.classList.add('blue');
+  caption.classList.add('hide');
+  bookList.classList.remove('active');
+  removeAllChildNodes(bookList);
+  addContainer.classList.remove('hide');
+  contactContainer.classList.add('hide');
 
-  let item;
+  // Add a book event listener
+  addbtn.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-  // Get the form values
-  bookTitleValue = bookTitle.value;
-  bookAuthorValue = bookAuthor.value;
+    let item;
 
-  if (bookTitleValue !== '' && bookAuthorValue !== '') {
-    errorMsg.innerText = '';
-    errorMsg.classList.remove('active2');
+    // Get the form values
+    bookTitleValue = bookTitle.value;
+    bookAuthorValue = bookAuthor.value;
 
-    item = { title: bookTitleValue, author: bookAuthorValue };
+    if (bookTitleValue !== '' && bookAuthorValue !== '') {
+      errorMsg.innerText = '';
+      errorMsg.classList.remove('active2');
 
-    Book.addBookItems(item);
+      item = { title: bookTitleValue, author: bookAuthorValue };
 
-    Book.addABook(item, bookArray.length - 1);
-  } else {
-    errorMsg.innerText = 'Please enter name of book & author\'s name.';
-    errorMsg.classList.add('active2');
-  }
-});
+      Book.addBookItems(item);
+
+      Book.addABook(item, bookArray.length - 1);
+    } else {
+      errorMsg.innerText = 'Please enter name of book & author\'s name.';
+      errorMsg.classList.add('active2');
+    }
+  });
+}
+
+// Contact link
+contact.onclick = () => {
+  list.classList.remove('blue');
+  addNew.classList.remove('blue');
+  contact.classList.add('blue');
+  bookList.classList.remove('active');
+  removeAllChildNodes(bookList);
+  addContainer.classList.add('hide');
+  contactContainer.classList.remove('hide');
+}
